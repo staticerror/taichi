4b75ab35106b7e852fe25e062dc473c86c99a01e
Your Name <john_mackay@live.com>
2010-06-16 14:24:32 +0530

    Added the notebook widget

--- gui.py
+++ gui.py
@@ -37,8 +37,12 @@ class MainWindow(wx.Frame):
         vbox = wx.BoxSizer(wx.VERTICAL)
         editortoolbar = self.EditorToolbar() #Editor actions, below menubar
         verticaltoolbar = self.VerticalToolbar() # the three main research, edit etc
-        vbox.Add(editortoolbar, 0 , wx.EXPAND)
-        vbox.Add(verticaltoolbar, 0, wx.EXPAND) 
+        
+        vbox.Add(editortoolbar, 0, wx.EXPAND) # editor, below menubar
+        
+
+        vbox.Add(verticaltoolbar, 10, wx.EXPAND | wx.RIGHT| wx.TOP| wx.BOTTOM, 20) 
+
 
         self.Bind(wx.EVT_TOOL, self.OnExit, id=wx.ID_EXIT)
         self.SetSizer(vbox)
@@ -49,7 +53,7 @@ class MainWindow(wx.Frame):
         
     def VerticalToolbar(self):
         
-        MainContainer = wx.GridBagSizer(2,2)
+        MainContainer = wx.BoxSizer(wx.HORIZONTAL)
 
         verticaltoolbar = wx.ToolBar(self, -1, style = wx.TB_VERTICAL )
         verticaltoolbar.AddLabelTool(-1, "Research", wx.Bitmap('/home/desktop/Desktop/crystal_project/64x64/apps/search.png'))
@@ -57,7 +61,11 @@ class MainWindow(wx.Frame):
         verticaltoolbar.AddLabelTool(-1, "Saved Articles", wx.Bitmap('/home/desktop/Desktop/crystal_project/64x64/filesystems/folder_red.png'))
         verticaltoolbar.Realize()
 
-        MainContainer.Add(verticaltoolbar, (5,0), flag = wx.EXPAND , border = 4)
+        MainContainer.Add(verticaltoolbar)
+        
+        notebook = self.ResearchNoteBook()
+        MainContainer.Add(notebook, 5,  wx.EXPAND | wx.ALL, 20)
+        
 
         return MainContainer
 
@@ -77,46 +85,48 @@ class MainWindow(wx.Frame):
 
     def ResearchNoteBook(self):
         
-        p = wx.Panel(self)
-        nb = wx.Notebook(p)
+        panel = wx.Panel(self)
+        nb = wx.Notebook(panel)
 
         # create the page windows as children of the notebook
-        page1 = PageOne(nb)
-        page2 = PageTwo(nb)
-        page3 = PageThree(nb)
-
+        page1 = ResearchPage(nb)
+        page2 = ResearchPage(nb)
         # add the pages to the notebook with the label to show on the tab
-        nb.AddPage(page1, "Page 1")
-        nb.AddPage(page2, "Page 2")
-        nb.AddPage(page3, "Page 3")
+        nb.AddPage(page1, "Article Directories")
+        nb.AddPage(page2, "Wikipedia")
+
 
         # finally, put the notebook in a sizer for the panel to manage
         # the layout
-        sizer = wx.BoxSizer()
-        sizer.Add(nb, 1, wx.EXPAND)
-        p.SetSizer(sizer)
+        sizer = wx.BoxSizer(wx.HORIZONTAL)
+        sizer.Add(nb, 10, wx.EXPAND)
+        panel.SetSizer(sizer)
+
+        return panel
 
 
 
 
 
-
-
-class ArtdirsPage(wx.Panel):
+class ResearchPage(wx.Panel):
     def __init__(self, parent):
         wx.Panel.__init__(self, parent)
-        t = wx.StaticText(self, -1, "This is a PageOne object", (20,20))
+        vbox = wx.BoxSizer(wx.VERTICAL)
+        hbox = wx.BoxSizer(wx.HORIZONTAL)
+        
+        textbox = wx.TextCtrl(self, -1 )
+        button = wx.Button(self, -1, 'Go!')
+        resultbox = wx.TextCtrl(self, -1,style =  wx.TE_MULTILINE | wx.TE_DONTWRAP)
+        hbox.Add(textbox, 4)
+        hbox.Add((35,-1))
+        hbox.Add(button , 1 ,  wx.ALIGN_RIGHT | wx.LEFT)
+        hbox.Add((155,-1))
 
-class WikipediaPage(wx.Panel):
-    def __init__(self, parent):
-        wx.Panel.__init__(self, parent)
-        t = wx.StaticText(self, -1, "This is a PageTwo object", (40,40))
+        
+        vbox.Add(hbox, 1, wx.EXPAND | wx.ALL, 20)
+        vbox.Add(resultbox, 7,  wx.EXPAND | wx.ALL , 20)
 
-class YAnswersPage(wx.Panel):
-    def __init__(self, parent):
-        wx.Panel.__init__(self, parent)
-        t = wx.StaticText(self, -1, "This is a PageThree object", (60,60))
-
+        self.SetSizer(vbox)
 
 
 
